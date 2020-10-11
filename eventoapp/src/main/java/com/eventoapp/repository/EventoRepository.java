bcec99be4b368b0993dd6b3d7a2e5692fa7a2a49
CarlosMacaneta <carlosmacaneta@hotmail.com>
2020-10-11 21:53:28 +0200

    Update EventoRepository.java

--- eventoapp/src/main/java/com/eventoapp/repository/EventoRepository.java
+++ eventoapp/src/main/java/com/eventoapp/repository/EventoRepository.java
@@ -8,5 +8,6 @@ import org.springframework.data.repository.CrudRepository;
  * @author CarlosMacaneta
  */
 public interface EventoRepository extends CrudRepository<Evento, String>{
-    
+    //buscando um evento especifico no banco de dados
+    Evento findbyId(long codigo);
 }
